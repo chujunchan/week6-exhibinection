abbecd771e082942db0baa0144ab0a724f5e2348
junchan <junchan0319@gmail.com>
2021-08-24 21:03:29 +0800

    Update 2021-08-24T13:03:21.802Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,5 +2,20 @@
 
 $(function () {
   console.log('Hello Bootstrap5');
+}); // swiper js
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
